d89b3cfdb631512d2d17745b906f9256f4b5a892
Andi <andicbuchanan@gmail.com>
2017-07-17 00:46:36 +0000

    todays prompt displaying

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var moment = require('moment');
+var mysql = require ('mysql');
+
+var today = moment().format("YYYY-MM-DD");
+
+var connection = mysql.createConnection({
+  host : '127.0.0.1',
+  user : 'andi_crb',
+  password : '',
+  database : 'c9'
+})
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Daily Words' });
+  console.log(today)
+  connection.query('SELECT * FROM Prompts WHERE Date = current_date', function(err, rows){
+    res.render('index', { title: 'Daily Words', prompts: rows });
 });
+})
 
 router.get('/faqs', function(req, res, next) {
   res.render('faqs', { title: 'Daily Words' });
